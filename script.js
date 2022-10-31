30eed22f8dd59ffc1648b6a5a2e78e24bb860468
Jack Sebben <jacksebben11@hotmail.com>
2022-10-31 07:48:40 -0400

    I created the function that allows the computer to select rock, paper or scissor at random. I've done this by using the Math oject with .random method.
    The Math.floor rounds a number down from a floating point, then Math.random selects a random number from our array of choices and choices.length picks a random point in that array. Test in console and shown to work.

--- script.js
+++ script.js
@@ -6,14 +6,19 @@
 - computerInput
 - userInput */
 
-let choices = ["rock", "paper", "scissors"]
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
-function playRound(playerSelection, computerSelection) {
+console.log(getComputerChoice());
+
+/*function playRound(playerSelection, computerSelection) {
     // your code here!
   }
    
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
-
+*/
 

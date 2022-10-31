19d1d778f313c5c6af2e937b5f3645805195b436
Jack Sebben <jacksebben11@hotmail.com>
2022-10-31 07:01:01 -0400

    Starting here. This is the starting point TOP has given me. I've listed the variables (I think) I will be using.

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Variables:
+- userWin
+- computerWin
+- rounds
+- computerSelection
+- computerInput
+- userInput */
+
+let choices = ["rock", "paper", "scissors"]
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+

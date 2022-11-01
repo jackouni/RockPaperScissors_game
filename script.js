632ab047b94f28366228331e75acaf2b436eba6e
Jack Sebben <jacksebben11@hotmail.com>
2022-11-01 07:08:00 -0400

    I reworked the playerSelection variable so that the player's input would be case insensitive. I did this by creating a variable, playerInput = prompt() and then storing the answer in the variable, playerSelection.toLowerCase().

--- script.js
+++ script.js
@@ -6,40 +6,46 @@
 - playerSelection */
 
 const choices = ["rock", "paper", "scissors"];
+let playerWin ;
+let computerWin ;
+let roundCount ;
+const playerSelection = prompt(("Pick ---> Rock, Paper, or Scissors").toLowerCase);
+const computerSelection = getComputerChoice();
 
+console.log(playRound(playerSelection, computerSelection));
+
+// This function makes the computer choose 'rock', 'paper' or 'scissors' at random.
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-let playWin 
+// This Function plays a full game of RPS consisting of 5 rounds (best of 5).
+function game(){
+    for (let roundCount = 0; roundCount < 5; roundCount++) {
+        return 
+    }
+}
 
 
-
+// This Function decides a winner for the round RPS
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "rock" && computerSelection === "scissors") 
     || (playerSelection === "scissors" && computerSelection === "paper")
     || (playerSelection === "paper" && computerSelection === "rock")) {
-        console.log("PlayerWin")
-        return i++
-    }
-    else if ((playerSelection === "rock" && computerSelection === "paper") 
+        console.log("PlayerWin") ; 
+        return playerWin ;
+    } else if 
+       ((playerSelection === "rock" && computerSelection === "paper") 
     || (playerSelection === "scissors" && computerSelection === "rock")
     || (playerSelection === "paper" && computerSelection === "scissors")) {
-        console.log("ComputerWin")
-        return i++
-    }
-    
-    else if (playerSelection === computerSelection){
-        console.log("Tie")
-        return i
-    }
-    
-
-  }
+        console.log("CompWin") ;
+        return compWin 
+    } else if (playerSelection === computerSelection){
+        console.log("Tie") ;
+        return tiedRound ;
+    } 
+}
    
-  let playerInput = "SCISSORS"
-  const playerSelection = playerInput.toLowerCase();
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
 
 

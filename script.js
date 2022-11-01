e66142a7aeb3e465081b58f2f1bed3e483d7c599
Jack Sebben <jacksebben11@hotmail.com>
2022-11-01 08:31:10 -0400

    I think I've screwed something up in the within VS code's settings and/or incorrectly used git trying to revert/reset to a previous point, there's an error I can't understand (InstallTrigger is deprecated and will be removed in the future.) and I think I may create another file and start a new.

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-/* Variables:
-- userWin
-- computerWin
-- rounds
-- computerSelection
-- playerSelection */
+let choices = ["rock", "paper", "scissors"]
+let playerInput = prompt('Pick ---> rock, paper, or scissors')
+let playerSelection = playerInput.toLowerCase;
+let computerSelection = getComputerChoice();
 
-const choices = ["rock", "paper", "scissors"];
-let playerWin ;
-let computerWin ;
-let roundCount ;
-const playerSelection = prompt(("Pick ---> Rock, Paper, or Scissors").toLowerCase);
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
-
-// This function makes the computer choose 'rock', 'paper' or 'scissors' at random.
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
-}
-
-// This Function plays a full game of RPS consisting of 5 rounds (best of 5).
-function game(){
-    for (let roundCount = 0; roundCount < 5; roundCount++) {
-        return 
+function playRound(playerSelection, computerSelection){
+    if ((playerSelection === "rock" && computerSelection === "scissors") 
+    || (playerSelection === "paper" && computerSelection === "rock")
+    || (playerSelection === "scissors" && computerSelection === "paper")) {
+        console.log("playerwin") ;
+        //return playerWin
+    } else if ((playerSelection === "rock" && computerSelection === "paper") 
+    || (playerSelection === "paper" && computerSelection === "scissors")
+    || (playerSelection === "scissors" && computerSelection === "rock")) {
+        console.log("compwin") ;
+        //return compWin
+    } else if ((playerSelection === "rock" && computerSelection === "rock") 
+    || (playerSelection === "paper" && computerSelection === "paper")
+    || (playerSelection === "scissors" && computerSelection === "scissors")) {
+        console.log("tied") ;
+        //return tied 
     }
 }
 
 
-// This Function decides a winner for the round RPS
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") 
-    || (playerSelection === "scissors" && computerSelection === "paper")
-    || (playerSelection === "paper" && computerSelection === "rock")) {
-        console.log("PlayerWin") ; 
-        return playerWin ;
-    } else if 
-       ((playerSelection === "rock" && computerSelection === "paper") 
-    || (playerSelection === "scissors" && computerSelection === "rock")
-    || (playerSelection === "paper" && computerSelection === "scissors")) {
-        console.log("CompWin") ;
-        return compWin 
-    } else if (playerSelection === computerSelection){
-        console.log("Tie") ;
-        return tiedRound ;
-    } 
-}
-   
+function getComputerChoice(){
+    return (choices[Math.floor(Math.random() * choices.length)]) ;  
+  }
 
+console.log(playRound(playerSelection, computerSelection));
 
-
+  
